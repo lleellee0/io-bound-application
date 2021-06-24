a360dcfd35c910efd4fdb53280dc71025e4e15ab
Hae-gun <newchk610@gmail.com>
2021-06-24 22:01:27 +0900

    c

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -19,7 +19,7 @@ public class PostController {
     @GetMapping("/test")
     public Post test(){
         Post p = new Post();
-        p.setId(3l);
+        p.setId(34l);
         return p;
     }
 //    docker build -t chkchk610/spring-boot-io-application .
