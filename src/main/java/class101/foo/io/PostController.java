f8233fbd8290a66e1513e8dc18e5b6e3bacb4e57
Hae-gun <newchk610@gmail.com>
2021-06-24 22:19:23 +0900

    feat:글번호로조회기능추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -30,10 +30,11 @@ public class PostController {
                 PageRequest.of(page -1 , PAGE_SIZE, Sort.by("id").descending())
         );
     }
-
-
     // 3. 글 번호로 조회
-    
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable("id") Long id){
+        return postRepository.findById(id).get();
+    }
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
 }
