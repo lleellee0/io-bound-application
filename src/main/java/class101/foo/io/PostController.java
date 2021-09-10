2e93ca284f40115eadd86149d8526281425aaeb2
dmee <dmeewg@gmail.com>
2021-09-10 20:45:58 +0900

    feat: 글번호로 조회기능 추가
    Took 2 minutes

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -30,8 +30,12 @@ public class PostController {
         );
     }
     
-
     // 3. 글 번호로 조회
+    @GetMapping("/post/{id}")
+    public Post getPostById(@PathVariable("id") Long id)
+    {
+        return postRepository.findById(id).get();
+    }
     
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
 
