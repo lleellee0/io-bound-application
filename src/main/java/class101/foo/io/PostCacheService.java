77c6c6a2e4cac27b657989a8811075a326c21ee1
kmnine3 <kmnine3@gmail.com>
2022-06-04 21:38:56 +0900

    feat: 캐시 추가

---
+++ src/main/java/class101/foo/io/PostCacheService.java
@@ -0,0 +1,25 @@
+package class101.foo.io;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class PostCacheService {
+    @Autowired
+    PostRepository postRepository;
+
+    private Page<Post> firstPostPage;
+
+    @Scheduled(cron = "* * * * * *")
+    public void updateFirstPostPage() {
+        firstPostPage = postRepository.findAll(
+                PageRequest.of(0, 20, Sort.by("id").descending())
+        );
+    }
+
+    public Page<Post> getFirstPostPage() {
+        return this.firstPostPage;
+    }
+}
