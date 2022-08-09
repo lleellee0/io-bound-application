80aff0b9dcf8c0bdc181e1c9fc8b40d040901bed
FKgk <rhkd865@gmail.com>
2022-08-09 21:56:30 +0900

    feat: 첫 페이지 캐싱

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -24,6 +24,9 @@ public class PostController {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    PostCacheService postCacheService;
+
     // 1. 글을 작성한다.
     @PostMapping("/post")
     public Post createPost(@RequestBody Post post) throws JsonProcessingException {
@@ -35,9 +38,13 @@ public class PostController {
     // 2. 글 목록을 페이징하여 반환.
     @GetMapping("/posts")
     public Page<Post> getPostList(@RequestParam(defaultValue = "1") Integer page) {
-        return postRepository.findAll(
-            PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
-        );
+        if(page.equals(1)){
+            return postCacheService.getFirstPostPage();
+        } else {
+            return postRepository.findAll(
+                PageRequest.of(page - 1, PAGE_SIZE, Sort.by("id").descending())
+            );
+        }
     }
     
     // 3. 글 번호로 조회
