c3bdbac9cba925fffe3fe15d22f11fda1152a4f8
sangho235 <sangho235@it.gsshop.com>
2023-02-17 00:18:53 +0900

    feat : 글 내용으로 검색 추가

--- src/main/java/class101/foo/io/PostRepository.java
+++ src/main/java/class101/foo/io/PostRepository.java
@@ -3,6 +3,9 @@ package class101.foo.io;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByContentContains(String content);
 }
